d6a429bc5810bc260f203c2f9bce2350c7f54d1b
Blaine Schmeisser <blaine.schmeisser@vitals.com>
2012-11-23 15:34:07 -0600

    Add sample unit test

---
+++ app/tests/cases/SampleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app\tests\cases;
+
+use li3_unit\test\Unit;
+
+class SampleTest extends Unit {
+
+	public function testSomething() {
+		$this->assertCount(3, array('foo', 'bar', 'baz'));
+	}
+
+}
+?>
